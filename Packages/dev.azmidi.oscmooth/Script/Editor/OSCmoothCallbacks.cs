245e860b903fdd9ce67bbbee19954dbf1dc8b3d1
regzo2 <regzo252@gmail.com>
2023-12-01 00:02:00 -0500

    Updated callbacks

--- Packages/dev.azmidi.oscmooth/Script/Editor/OSCmoothCallbacks.cs
+++ Packages/dev.azmidi.oscmooth/Script/Editor/OSCmoothCallbacks.cs
@@ -1,98 +1,113 @@
-﻿using OSCmooth.Animation;
-using static VRC.SDK3.Avatars.Components.VRCAvatarDescriptor;
+﻿using static VRC.SDK3.Avatars.Components.VRCAvatarDescriptor;
 using UnityEditor;
 using UnityEngine;
 using VRC.SDKBase.Editor.BuildPipeline;
 using System.IO;
 using UnityEditor.Animations;
+using UnityEditor.Build;
+using VRC.SDK3.Avatars.Components;
+using OSCmooth.Editor.Animation;
+using OSCmooth.Util;
+using VRC.SDK3.Avatars.ScriptableObjects;
+using System.Linq;
+using OSCmooth.Types;
+using System.Collections.Generic;
 
 namespace OSCmooth.Editor
 {
-    public class OSCmoothPostprocessor : IVRCSDKPostprocessAvatarCallback
+    public class OSCmoothPostprocessor : IVRCSDKPostprocessAvatarCallback, IVRCSDKOrderedCallback, IOrderedCallback
     {
-        public int callbackOrder => 1024;
+        public int callbackOrder => 0;
 
         public void OnPostprocessAvatar()
         {
-            var _oscmBehaviors = Object.FindObjectsOfType<OSCmoothBehavior>();
-            foreach (var oscm in _oscmBehaviors)
-                oscm.avatarDescriptor.baseAnimationLayers = oscm.prevLayers;
+            OSCmoothBehavior[] _oscmBehaviors = Object.FindObjectsOfType<OSCmoothBehavior>();
+            foreach (OSCmoothBehavior oscm in _oscmBehaviors)
+            {
+                VRCAvatarDescriptor _avatarDescriptor = oscm.GetComponent<VRCAvatarDescriptor>();
+                _avatarDescriptor.baseAnimationLayers = oscm.prevLayers;
+                _avatarDescriptor.expressionParameters.parameters = oscm.prevParameters;
+            }
         }
     }
 
-    public class OSCmoothPreprocessor : IVRCSDKBuildRequestedCallback
-    {
-        public int callbackOrder => -1024;
-        public bool OnBuildRequested(VRCSDKRequestedBuildType type)
-        {
-            if (type != VRCSDKRequestedBuildType.Avatar) return true;
+	public class OSCmoothPreprocessor : IVRCSDKBuildRequestedCallback, IVRCSDKOrderedCallback, IOrderedCallback
+	{
+		public int callbackOrder => 0;
 
-            var _oscmBehaviors = Object.FindObjectsOfType<OSCmoothBehavior>();
+		public bool OnBuildRequested(VRCSDKRequestedBuildType type)
+		{
+			if ((int)type > 0)
+			{
+				return true;
+			}
+			OSCmoothBehavior[] _oscmBehaviors = Object.FindObjectsOfType<OSCmoothBehavior>();
+			OSCmoothBehavior[] array = _oscmBehaviors;
+			foreach (OSCmoothBehavior oscm in array)
+			{
+				if (oscm == null)
+				{
+					return false;
+				}
+				VRCAvatarDescriptor _avatarDescriptor = oscm.GetComponent<VRCAvatarDescriptor>();
+				oscm.prevLayers = (CustomAnimLayer[])(object)new CustomAnimLayer[_avatarDescriptor.baseAnimationLayers.Length];
+				for (int j = 0; j < _avatarDescriptor.baseAnimationLayers.Length; j++)
+				{
+					oscm.prevLayers[j] = _avatarDescriptor.baseAnimationLayers[j];
+				}
+				oscm.prevParameters = (VRCExpressionParameters.Parameter[])(object)new VRCExpressionParameters.Parameter[_avatarDescriptor.expressionParameters.parameters.Length];
+				for (int i = 0; i < _avatarDescriptor.expressionParameters.parameters.Length; i++)
+				{
+					oscm.prevParameters[i] = _avatarDescriptor.expressionParameters.parameters[i];
+				}
+				if (!oscm.setup.parameters.AppendToExpressionParameters(_avatarDescriptor))
+				{
+					return false;
+				}
+				ApplyAnimationLayers(_avatarDescriptor, oscm);
+			}
+			AssetDatabase.SaveAssets();
+			return true;
+		}
 
-            foreach (var oscm in _oscmBehaviors)
-            {
-                if (oscm == null)
-                    return false;
+		public static void ApplyAnimationLayers(VRCAvatarDescriptor avatarDescriptor, OSCmoothBehavior behavior)
+		{
+			CustomAnimLayer[] layers = avatarDescriptor.baseAnimationLayers;
+			for (int i = 0; i < layers.Length; i++)
+			{
+				if (layers[i].animatorController == null)
+					continue;
 
-                var _avatarDescriptor = oscm.avatarDescriptor;
-                if (_avatarDescriptor == null)
-                    return false;
+				string _animatorPath = AssetDatabase.GetAssetPath(layers[i].animatorController);
+				AnimLayerType _type = layers[i].type;
+				List<OSCmoothParameter> _layerParameters = behavior.setup.parameters
+					.Where(p => Extensions.Contains(p.layerMask, _type)).ToList();
 
-                oscm.prevLayers = _avatarDescriptor.baseAnimationLayers;
-                if (oscm.prevLayers == null)
-                    return false;
+				string _directory = "Assets/OSCmooth/Temp/";
+				if (!Directory.Exists(_directory))
+				{
+					Directory.CreateDirectory(_directory);
+					Debug.Log("Directory created: " + _directory);
+				}
 
-                var _newLayers = new CustomAnimLayer[_avatarDescriptor.baseAnimationLayers.Length];
-                for (int i = 0; i < _avatarDescriptor.baseAnimationLayers.Length; i++)
-                {
-                    _newLayers[i] = ApplyAnimationLayer(oscm, i);
-                }
-                _avatarDescriptor.baseAnimationLayers = _newLayers;
-            }
-            return true;
-        }
+				string _proxyPath = $"{_directory}{layers[i].type}Proxy.controller";
+				if (!AssetDatabase.CopyAsset(_animatorPath, _proxyPath)) continue;
 
-        private CustomAnimLayer ApplyAnimationLayer(OSCmoothBehavior oscm, int i)
-        {
-            var avatarDescriptor = oscm.avatarDescriptor;
-            var _animatorPath = AssetDatabase.GetAssetPath(avatarDescriptor.baseAnimationLayers[i].animatorController);
+				AssetDatabase.SaveAssets();
+				AnimatorController _layerProxyController = AssetDatabase.LoadAssetAtPath<AnimatorController>(_proxyPath);
+				string _proxyGUID = AssetDatabase.AssetPathToGUID(_proxyPath);
+				new OSCmoothAnimationHandler
+				(
+					_layerParameters,
+					_layerProxyController,
+					_directory + "Generated/Smooth/Animator_" + _proxyGUID + "/",
+					_directory + "Generated/Binary/Animator_" + _proxyGUID + "/")
+				.CreateLayers();
 
-            string directory = "Assets/OSCmooth/Temp/";
-
-            if (!Directory.Exists(directory))
-            {
-                Directory.CreateDirectory(directory);
-                Debug.Log($"Directory created: {directory}");
-            }
-
-            string _proxyPath = $"{directory}{avatarDescriptor.baseAnimationLayers[i].type}Proxy.controller";
-            bool success = AssetDatabase.CopyAsset(_animatorPath, _proxyPath);
-            AssetDatabase.SaveAssets();
-
-            if (success)
-            {
-                var _proxyController = AssetDatabase.LoadAssetAtPath<AnimatorController>(_proxyPath);
-                var _animatorGUID = AssetDatabase.AssetPathToGUID(_proxyPath);
-                var _animationHandler = new OSCmoothAnimationHandler();
-
-                _animationHandler._animatorController = _proxyController;
-                _animationHandler._parameters = oscm.setup.layers[i].parameters;
-                _animationHandler._animExportDirectory = "Assets/OSCmooth/Temp/Generated/Smooth/" + "Animator_" + _animatorGUID + "/";
-                _animationHandler._binaryExportDirectory = "Assets/OSCmooth/Temp/Generated/Binary/" + "Animator_" + _animatorGUID + "/";
-                //_animationHandler.RemoveAllOSCmoothFromController();
-                _animationHandler.CreateLayers();
-
-                return new CustomAnimLayer
-                {
-                    type = avatarDescriptor.baseAnimationLayers[i].type,
-                    animatorController = _proxyController,
-                    isEnabled = avatarDescriptor.baseAnimationLayers[i].isEnabled,
-                    isDefault = avatarDescriptor.baseAnimationLayers[i].isDefault,
-                    mask = avatarDescriptor.baseAnimationLayers[i].mask
-                };
-            }
-            return avatarDescriptor.baseAnimationLayers[i];
-        }
-    }
+				layers[i].animatorController = _layerProxyController;
+			}
+			avatarDescriptor.baseAnimationLayers = layers;
+		}
+	}
 }
 
